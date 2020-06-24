e90005f5371e4c601b2f521fba10cc1d9156ce08
Markus Hofbauer <markus.hofbauer@tum.de>
2020-06-24 13:49:09 +0200

    add superlint

--- devsync/parser.py
+++ devsync/parser.py
@@ -7,25 +7,27 @@ from devsync.log import logger
 
 
 class YMLConfigParser:
-
     def __init__(self, config):
         self.__content = self.get_yaml_content(config)
 
     @staticmethod
     def get_yaml_content(filename):
-        with open(filename, 'r') as stream:
+        with open(filename, "r") as stream:
             return yaml.load(stream)
 
     def parse_backup_folder(self):
-        backup_folders = self.__content['backupFolder']
-        return [BackupFolder(self.parse_home(), element['path']) for element in backup_folders]
+        backup_folders = self.__content["backupFolder"]
+        return [
+            BackupFolder(self.parse_home(), element["path"])
+            for element in backup_folders
+        ]
 
     @staticmethod
     def parse_targets(short_dest_file):
         content = YMLConfigParser.get_yaml_content(short_dest_file)
         if content:
-            targets = content['shortDests']
-            return {target['shortDest']: target['destPath'] for target in targets}
+            targets = content["shortDests"]
+            return {target["shortDest"]: target["destPath"] for target in targets}
 
         return {}
 
@@ -36,10 +38,14 @@ class YMLConfigParser:
 
         targets = YMLConfigParser.parse_targets(short_dest_file)
         if target_arg in targets:
-            logger.info('Using shortcut "{}" as destination path to: {}\n'.format(target_arg, targets[target_arg]))
+            logger.info(
+                'Using shortcut "{}" as destination path to: {}\n'.format(
+                    target_arg, targets[target_arg]
+                )
+            )
             return Target(targets[target_arg])
         else:
             return Target(target_arg)
 
     def parse_home(self):
-        return self.__content['home']
+        return self.__content["home"]
--- devsync/sync.py
+++ devsync/sync.py
@@ -12,26 +12,26 @@ def run_backup(parser, target, last_update, report):
     for backup_folder in backup_folders:
         backup_folder.find_repos_in_path()
 
-    logger.info('Updating Repos...\n')
+    logger.info("Updating Repos...\n")
     repo_sync = RepoSync(home, backup_folders)
     repo_sync.update_repos(target, last_update, report)
 
-    logger.info('Sync data with rsync...\n')
+    logger.info("Sync data with rsync...\n")
     rsync = RSync(home, backup_folders)
     rsync.sync(target, report)
 
 
 class RSync:
     OPTIONS = [
-        '-a',                                   # Make 1 to 1 copy
-        '-v',                                   # Verbose
-        '--whole-file',                         # Copy whole file without delta algorithm
-        '--delete',                             # Delete if not existing in root
-        '--stats',                              # Show file transfer stats
-        '--progress',                           # Show Progress
-        '--modify-window=2',                    # Allowing times to differ by up to 2 seconds. Related to FAT 2 second
-                                                # resolution for timestamps
-        '--log-file={}'.format(LOGFILE)         # Log to LOGFILE
+        "-a",  # Make 1 to 1 copy
+        "-v",  # Verbose
+        "--whole-file",  # Copy whole file without delta algorithm
+        "--delete",  # Delete if not existing in root
+        "--stats",  # Show file transfer stats
+        "--progress",  # Show Progress
+        "--modify-window=2",  # Allowing times to differ by up to 2 seconds. Related to FAT 2 second
+        # resolution for timestamps
+        "--log-file={}".format(LOGFILE),  # Log to LOGFILE
     ]
 
     def __init__(self, root, backup_folders):
@@ -42,38 +42,48 @@ class RSync:
     def get_options(report, excludes):
         options = copy.deepcopy(RSync.OPTIONS)
         if report:
-            options.append('-n')  # Rsync Dry Run
+            options.append("-n")  # Rsync Dry Run
         for path in excludes:
             options.append('--exclude="{}"'.format(path))
 
-        return ' '.join(options)
+        return " ".join(options)
 
     def sync(self, target, report):
         for element in self.__backup_folders:
             excludes = element.get_relative_repo_paths()
             options = self.get_options(report, excludes)
 
-            logger.verbose(str(len(excludes)) + ' Repos to exclude in ' + element.path)
-            logger.debug('Running Rsync' +
-                         '\n\tSource: ' + element.path +
-                         '\n\tTarget: ' + target.path +
-                         '\n\tOptions: ' + options + '\n')
-            subprocess.check_call('rsync {} {} {}'.format(options, element.path, target.path), shell=True,
-                                  cwd=self.__root)
+            logger.verbose(str(len(excludes)) + " Repos to exclude in " + element.path)
+            logger.debug(
+                "Running Rsync"
+                + "\n\tSource: "
+                + element.path
+                + "\n\tTarget: "
+                + target.path
+                + "\n\tOptions: "
+                + options
+                + "\n"
+            )
+            subprocess.check_call(
+                "rsync {} {} {}".format(options, element.path, target.path),
+                shell=True,
+                cwd=self.__root,
+            )
 
 
 class RepoSync:
-
     def __init__(self, root, backup_folders):
         self.__root = root
         self.__backup_folders = backup_folders
 
     def update_repos(self, target, last_update, report):
         all_repos = self.get_all_repos()
-        logger.verbose(str(len(all_repos)) + ' Repos found in all paths')
+        logger.verbose(str(len(all_repos)) + " Repos found in all paths")
 
         all_repos = [repo for repo in all_repos if repo.is_update_required(last_update)]
-        logger.verbose('{} Repos to update on target {}\n'.format(len(all_repos), target.path))
+        logger.verbose(
+            "{} Repos to update on target {}\n".format(len(all_repos), target.path)
+        )
 
         for repo in all_repos:
             repo.update_repo_on_target(self.__root, target, report)
@@ -82,7 +92,9 @@ class RepoSync:
         all_repos = []
         for element in self.__backup_folders:
             repos = element.repos
-            logger.verbose('--> ' + str(len(repos)) + ' Repos found in path ' + element.path)
+            logger.verbose(
+                "--> " + str(len(repos)) + " Repos found in path " + element.path
+            )
             all_repos.extend(repos)
 
         return all_repos
