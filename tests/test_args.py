179a73da3071d8146c8beaf82a91033b7614b1ca
Markus Hofbauer <markus.hofbauer@tum.de>
2023-07-18 16:00:00 +0200

    convert few tests to pytest

--- tests/test_args.py
+++ tests/test_args.py
@@ -2,26 +2,23 @@
 
 from pathlib import Path
 
-from pyfakefs.fake_filesystem_unittest import TestCase
+import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
 
 from devsync.args import dir_path
 
 
-class ArgsTest(TestCase):
-    """Args test."""
+def test_dir_path__is_dir__input_path(fs: FakeFilesystem) -> None:
+    fs.create_dir("test")
+    assert Path("test") == dir_path("test")
 
-    def setUp(self) -> None:
-        self.setUpPyfakefs()
 
-    def test_dir_path__is_dir__input_path(self):
-        self.fs.create_dir("test")
-        self.assertEqual(Path("test"), dir_path("test"))
+def test_dir_path__is_file__raise_not_a_directory_error(fs: FakeFilesystem) -> None:
+    fs.create_file("test")
+    with pytest.raises(NotADirectoryError):
+        dir_path("test")
 
-    def test_dir_path__is_file__raise_not_a_directory_error(self):
-        self.fs.create_file("test")
-        with self.assertRaises(NotADirectoryError):
-            dir_path("test")
 
-    def test_dir_path__does_not_exist__raise_not_a_directory_error(self):
-        with self.assertRaises(NotADirectoryError):
-            dir_path("test")
+def test_dir_path__does_not_exist__raise_not_a_directory_error() -> None:
+    with pytest.raises(NotADirectoryError):
+        dir_path("foo")
--- tests/test_log.py
+++ tests/test_log.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 
-from pyfakefs.fake_filesystem_unittest import TestCase
+from pyfakefs.fake_filesystem import FakeFilesystem
 
 from devsync.log import init_logging
 
 
-class LogTest(TestCase):
-    def setUp(self) -> None:
-        self.setUpPyfakefs()
-
-    def test_init_logging___logfile_path_does_not_exist__should_be_created(self):
-        init_logging(Path("logs/logfile"))
-        self.assertTrue(Path("logs/logfile").exists())
+def test_init_logging___logfile_path_does_not_exist__should_be_created(
+    fs: FakeFilesystem,
+) -> None:
+    init_logging(Path("logs/logfile"))
+    assert fs.exists("logs/logfile")
